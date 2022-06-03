be73c5087f24bf966572979dfacf9c05ca39c479
Joshua M <72484879+warybyte@users.noreply.github.com>
2022-06-03 16:15:35 -0400

    Update uuid-gen.sh

--- uuid-gen.sh
+++ uuid-gen.sh
@@ -1,29 +1,64 @@
 #!/bin/bash
-# I wrote this script because I hate guessing random values...and CAS service profiles want SEVERAL!
-# Key features:
-# xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx format UUID...this is standard in my environment, so YMMV
-# 16 char ServiceID...because we wouldn't want any confused services!
-# 4 char eval order...you probably want less in your env...I hope...
-# Auto-check existing profiles for duplication...no more grepping! Only errors on matches.
+# Script auto-generates data for CAS JSON service profiles.
+# UUID: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx format
+# ServiceID: aaaabbbbaaaabbbb
+# EvalOrder: abab
+#
+# Auto-check existing profiles for duplication. If a match is identified, the generator for
+# that value reruns until it pulls a unique value.
+#
+# Instructions:
+# ./uuid_gen.sh <profilename>
+#
+# generate a random UUID value for CAS service profiles
+casroot="etc/cas/"
+# UUID variables, split into five groups then combined
+val0=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 8 | head -1 | tr [:upper:] [:lower:]);
+val1=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 4 | head -1 | tr [:upper:] [:lower:]);
+val2=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1);
+val3=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1);
+val4=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 12 | head -1 | tr [:upper:] [:lower:]);
+uuid="$val0-$val1-$val2-$val3-$val4";
 
-# build UUID
-uval0=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 8 | head -1 | tr [:upper:] [:lower:])
-uval1=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 4 | head -1 | tr [:upper:] [:lower:])
-uval2=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1)
-uval3=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1)
-uval4=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 12 | head -1 | tr [:upper:] [:lower:])
-uuid="$uval0-$uval1-$uval2-$uval3-$uval4"
+serviceid=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 16 | head -1);
+evalorder=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1);
 
-# set the other vars
-serviceid=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 16 | head -1)
-evalorder=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1)
+# set and check UUID
+grep -r "$uuid" $casroot 2>/dev/null;
+valuecheck=$?;
+while [ $valuecheck -eq 0 ]
+do
+	echo "Recaculating UUID";
+	val4=$(cat /dev/urandom | tr -dc [:alnum:] | fold -w 12 | head -1 | tr [:upper:] [:lower:]);
+	uuid="$val0-$val1-$val2-$val3-$val4";
+	grep -r "$uuid" $casroot 2>/dev/null;
+        valuecheck=$?;
+done
 
-# output
+# set and check ServiceID
+grep -r "$serviceid" $casroot 2>/dev/null;
+valuecheck=$?;
+while [ $valuecheck -eq 0 ]
+do
+        echo "Recaculating ServiceID";
+        serviceid=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 16 | head -1)
+        grep -r "$serviceid" $casroot 2>/dev/null;
+        valuecheck=$?;
+done
+
+#set and check EvaluationOrder
+grep -r "\"evaluationOrder\": $evalorder" $casroot 2>/dev/null;
+valuecheck=$?;
+while [ $valuecheck -eq 0 ]
+do
+        echo "Recaculating EvaluationOrder";
+        evalorder=$(cat /dev/urandom | tr -dc [:digit:] | fold -w 4 | head -1)
+        grep -r "\"evaluationOrder\": $evalorder" $casroot 2>/dev/null;
+        valuecheck=$?;
+done
+
+# Print results
 echo "UUID: $uuid";
-echo "ServiceID: $serviceid";
-# check for dups
-grep -r "$uuid\|$serviceid" /etc/cas/ 2>/dev/null;
-
 echo "EvaluationOrder: $evalorder";
-# check for dups
-grep -r "\"evaluationOrder\": $evalorder" 2>/dev/null;
+echo "ServiceID: $serviceid";
+echo "FileName: $1_$evalorder-$serviceid.json";
